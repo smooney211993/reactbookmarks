02fc353907a0b0503b57947b6b409065e3c71ed1
smooney211993 <sm.athome21@gmail.com>
2020-08-26 14:41:29 +1000

    added error messages

---
+++ src/Helpers/passwordvalidation.js
@@ -0,0 +1,10 @@
+const validatePassword = (password) =>{
+    const expression = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/g
+    const regex = new RegExp(expression);
+    if(!password.match(regex)){
+        return false;
+    }
+    return true;
+}
+
+export default validatePassword; 
